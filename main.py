525313f7ee264361774b0de99726a2e6d61551d0
abdbot-sf <61231158+abdbot-sf@users.noreply.github.com>
2020-03-13 16:34:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import numpy as np
-def chislo_x(number):
-    n=0
-    max1=100
-    min1=0
-    ugadai=0
-    while True:
-        ugadai=int((max1+min1)/2)
-        if ugadai==number:
-            break
-        elif ugadai>number:
-            max1=ugadai
-        elif ugadai<number:
-            min1=ugadai
-        n+=1
-    return n
+Print("hello world")
