938645ae435224fa34cc1f10b01a7d768cad80a9
abdbot-sf <aabdraimova@fortebank.com>
2020-03-13 16:22:56 +0600

    1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import numpy as np
+def chislo_x(number):
+    n=0
+    max1=100
+    min1=0
+    ugadai=0
+    while True:
+        ugadai=int((max1+min1)/2)
+        if ugadai==number:
+            break
+        elif ugadai>number:
+            max1=ugadai
+        elif ugadai<number:
+            min1=ugadai
+        n+=1
+    return n
